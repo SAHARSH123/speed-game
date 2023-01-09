587dbbc0acefbe457f16e3b5107afcf750a1186a
saharshjain506@gmail.com <saharshjain506@gmail.com>
2023-01-09 16:00:30 +0530

    Introduced Hooks

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import { useWord } from "./hooks/useWord";
 export default function App() {
-  const [textValue, setTextValue] = useState("");
-  const [totalWords, settotalWords] = useState(0);
-  const [timeRemaining, setTimeRemaining] = useState(7);
-  const [gameStarted, setgameStarted] = useState(false);
-  const textRef = useRef(null);
-
-  useEffect(() => {
-    if (timeRemaining > 0 && gameStarted) {
-      setTimeout(() => {
-        setTimeRemaining((time) => time - 1);
-      }, 1000);
-    } else if (timeRemaining === 0 && gameStarted) {
-      endGame();
-    } else if (gameStarted) {
-    }
-  }, [timeRemaining, gameStarted]);
-
-  const calculateWordsCount = () => {
-    const words = textValue.trim().split(" ");
-    const filteredArray = words.filter((word) => word !== "");
-    return filteredArray.length;
-  };
-
-  const endGame = () => {
-    setgameStarted(false);
-    settotalWords(calculateWordsCount());
-  };
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setTextValue(value);
-  };
-
-  const startClock = (e) => {
-    setgameStarted(true);
-    setTimeRemaining(7);
-    setTextValue("");
-    settotalWords(0);
-    textRef.current.disabled = false;
-    textRef.current.focus();
-  };
-
+  const {
+    textValue,
+    gameStarted,
+    textRef,
+    handleChange,
+    timeRemaining,
+    startClock,
+    totalWords,
+  } = useWord();
   return (
     <>
       <h1>How Fast do you type?</h1>
